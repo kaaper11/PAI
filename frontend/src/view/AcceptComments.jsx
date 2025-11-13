a2e128576f7e4938a88c6579519816c3e7793a3a
Kacper Ajdyna <ajdynakacper@gmail.com>
2025-11-13 18:33:28 +0100

    Del

---
+++ frontend/src/view/AcceptComments.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+
+
+
+const AcceptComments = () => {
+    return(
+        <div></div>
+    )
+}
+export default AcceptComments;
